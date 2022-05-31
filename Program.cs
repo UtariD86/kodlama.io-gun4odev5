1e0e2d67fcbf10c01443029f78e958c9f11060c7
UtariD86 <69503659+UtariD86@users.noreply.github.com>
2022-05-31 18:23:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UtariDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UtariDict<string, string> dict = new UtariDict<string, string>();
+
+            dict.Add("nissan", "skyline");
+            dict.Add("toyota", "trueno");
+            dict.Add("honda", "civic");
+            dict.Add("mazda", "miata");
+            dict.Add("mitsubishi", "lancer");
+            dict.Add("subaru", "impreza");
+            dict.Print("toyota");
+            dict.Print("subaru");
+            dict.Print("honda");
+            dict.Print();
+
+        }
+    }
+}
---
+++ UtariDict.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtariDictionary
+{
+    internal class UtariDict< Ukey,Uvalue >
+    {
+        Ukey[] keys;
+        Uvalue[] values;
+        public UtariDict()
+
+        {
+            keys = new Ukey[0];
+            values = new Uvalue[0];
+        }
+
+        public void Add(Ukey key, Uvalue value)
+        {
+            Ukey[] tempKeys = keys;
+            Uvalue[] tempValues = values;
+
+            keys = new Ukey[keys.Length + 1];
+            values = new Uvalue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public void Print(string input)
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                string keyCheck = keys[i].ToString();
+                
+                if (keyCheck == input)
+                {
+                    Console.WriteLine(values[i]);
+                }
+            }
+            
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " : " + values[i]);
+            }
+
+        }
+    }
+}
